aee507e4e17abe82bc88e4fb06787d3c302fa56e
Dhineshkannan97 <dhineshkannan.madhavan@gcitsolutions.com>
2022-10-31 19:12:03 +0530

    stage 1

---
+++ src/main/java/Deserialize.java
@@ -0,0 +1,22 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
+//public class Deserialize extends Postgres{
+//     public Deserializem(int id,
+//    String name,
+//    double cgpa,
+//    int age,
+//    boolean active) throws IOException, ClassNotFoundException {
+//         String loc = "C:\\Users\\User\\Downloads\\post12.txt";
+//         ObjectInputStream fin = new ObjectInputStream(new FileInputStream(loc));
+//         ArrayList<String> listin = new ArrayList<String>();
+//       Deserialize p= (Deserialize) fin.readObject();
+//       System.out.println(p.id+","+p.age+","+p.cgpa+","+p.name+","+p.active);
+//     }
+//
+//    public Deserialize() {
+//        super(fin.readObject());
+//    }
+//}
--- src/main/java/Postgres.java
+++ src/main/java/Postgres.java
@@ -1,17 +1,24 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.sql.*;
 import java.util.ArrayList;
 
 public class Postgres {
-    public static void main(String[] args) throws SQLException, IOException {
+
+    int id;
+    String name;
+    double cgpa;
+    int age;
+    boolean active;
+
+    public Postgres(Object readObject) {
+    }
+
+    public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
         Connection c;
         c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/document",
                 "postgres", "pg@abi123");
         PreparedStatement stmt = c.prepareStatement("SELECT * FROM student");
         ResultSet rs = stmt.executeQuery();
-        String s3 = null;
         ArrayList<String> list = new ArrayList<String>();
         while (rs.next()) {
 
@@ -25,19 +32,29 @@ public class Postgres {
                 var s2 = Double.toString(cgpa);
                 var name = rs.getString("name");
                 list.add(s + "," + s1 + "," + s2 + "," + active + "," + name);
-                System.out.println(s);
-                System.out.println(name);
-                System.out.println(s1);
-                System.out.println(s2);
-                System.out.println(active);
+//                list.
+//                System.out.println(s);
+//                System.out.println(name);
+//                System.out.println(s1);
+//                System.out.println(s2);
+//                System.out.println(active);
             }
         }
-        String loc = "C:\\Users\\User\\Downloads\\post11.txt";
-        File first = new File(loc);
-        FileWriter write = new FileWriter(first);
-        write.write(String.valueOf(list));
-        write.flush();
-        write.close();
+        String loc = "C:\\Users\\User\\Downloads\\post12.txt";
+        FileOutputStream first = new FileOutputStream(loc);
+        ObjectOutputStream out = new ObjectOutputStream(first);
+        out.writeObject(list);
 
+        ObjectInputStream fin = new ObjectInputStream(new FileInputStream(loc));
+//        ArrayList<String> listin = new ArrayList<String>();
+        Postgres p = new Postgres(fin.readObject());
+//        Postgres p= (Postgres) fin.readObject();
+        System.out.println(p.id + "," + p.age + "," + p.cgpa + "," + p.name + "," + p.active);
+        out.flush();
+        out.close();
+//        FileWriter write = new FileWriter(first);
+//        write.write(String.valueOf(list));
+//        write.flush();
+//        write.close();
     }
 }
