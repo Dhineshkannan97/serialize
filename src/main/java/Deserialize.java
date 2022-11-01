bb75d8bc0c9f06178e3ce2b98d5fe2e5e5f8ac3b
Dhineshkannan97 <dhineshkannan.madhavan@gcitsolutions.com>
2022-11-01 12:41:04 +0530

    stage 1

--- src/main/java/Deserialize.java
+++ src/main/java/Deserialize.java
@@ -3,20 +3,20 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
-//public class Deserialize extends Postgres{
-//     public Deserializem(int id,
-//    String name,
-//    double cgpa,
-//    int age,
-//    boolean active) throws IOException, ClassNotFoundException {
-//         String loc = "C:\\Users\\User\\Downloads\\post12.txt";
-//         ObjectInputStream fin = new ObjectInputStream(new FileInputStream(loc));
-//         ArrayList<String> listin = new ArrayList<String>();
-//       Deserialize p= (Deserialize) fin.readObject();
-//       System.out.println(p.id+","+p.age+","+p.cgpa+","+p.name+","+p.active);
-//     }
-//
-//    public Deserialize() {
-//        super(fin.readObject());
-//    }
-//}
+public class Deserialize extends Postgres{
+     public Deserializem(int id,
+    String name,
+    double cgpa,
+    int age,
+    boolean active) throws IOException, ClassNotFoundException {
+         String loc = "C:\\Users\\User\\Downloads\\post12.txt";
+         ObjectInputStream fin = new ObjectInputStream(new FileInputStream(loc));
+         ArrayList<String> listin = new ArrayList<String>();
+       Deserialize p= (Deserialize) fin.readObject();
+       System.out.println(p.id+","+p.age+","+p.cgpa+","+p.name+","+p.active);
+     }
+
+    public Deserialize() {
+        super(fin.readObject());
+    }
+}
